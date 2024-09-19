099cc6b253b1624cbdb2bee51fc28fa8bdb71495
MaxLarry <202080010@psu.palawan.edu.ph>
2024-09-19 14:26:31 +0800

    working with fetch property, count room, and frontend modification

--- backend_rencon/services/ListingRequest.services.js
+++ backend_rencon/services/ListingRequest.services.js
@@ -13,138 +13,6 @@ const getAllPendingRequestsWithProfiles = async () => {
       },
       {
         $lookup: {
-          from: "pending_request_profile", // Ensure this collection matches your database
-          localField: "userId",
-          foreignField: "userId",
-          as: "profile",
-        },
-      },
-      {
-        $unwind: {
-          path: "$profile",
-          preserveNullAndEmptyArrays: true, // Preserve listings without a matching profile
-        },
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "userId",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $unwind: {
-          path: "$user",
-          preserveNullAndEmptyArrays: true, // Preserve listings without a matching user
-        },
-      },
-      {
-        $lookup: {
-          from: "rooms", // Ensure this collection matches your database
-          localField: "_id", // Use the property ID to look up related rooms
-          foreignField: "propertyId",
-          as: "rooms",
-        },
-      },
-      {
-        $unwind: {
-          path: "$rooms",
-          preserveNullAndEmptyArrays: true, // Preserve listings without matching rooms
-        },
-      },
-      {
-        $project: {
-          _id: 1,
-          description: 1,
-          status: 1,
-          created_at: 1,
-          typeOfProperty: 1,
-          location: 1,
-          address: {
-            $concat: ["$street", ", ", "$barangay", ", ", "$city"], // Concatenate firstName and lastName
-          },
-          amenities: {
-            $cond: {
-              if: { $isArray: "$amenities" },
-              then: "$amenities",
-              else: [],
-            },
-          },
-          property_photo: {
-            $filter: {
-              input: ["$photo", "$photo2", "$photo3"],
-              as: "photo",
-              cond: { $ne: ["$$photo", null] }, // Exclude null photos
-            },
-          }, // Project property photos
-          legal_docs: {
-            $filter: {
-              input: ["$legalDocPhoto", "$legalDocPhoto2", "$legalDocPhoto3"],
-              as: "legaldocsPhoto",
-              cond: { $ne: ["$$legaldocsPhoto", null] }, // Exclude null room photos
-            }, // Project legal document photos
-          },
-          profile: {
-            email: "$user.email",
-            fullName: {
-              $concat: ["$profile.firstName", " ", "$profile.lastName"], // Concatenate firstName and lastName
-            },
-            contactDetails: "$profile.contactDetails",
-          },
-          rooms: {
-            roomId: "$rooms._id", // Project room details
-            roomNumber: "$rooms.roomNumber",
-            roomPhoto: {
-              $filter: {
-                input: ["$rooms.photo1", "$rooms.photo2", "$rooms.photo3"],
-                as: "roomPhoto",
-                cond: { $ne: ["$$roomPhoto", null] }, // Exclude null room photos
-              },
-            },
-            capacity: "$rooms.capacity",
-            price: "$rooms.price",
-            deposit: "$rooms.deposit",
-            advance: "$rooms.advance",
-            availability: "$rooms.availability",
-          },
-        },
-      },
-      {
-        $group: {
-          _id: "$_id", // Group by property ID to combine rooms into an array
-          description: { $first: "$description" },
-          amenities: { $first: "$amenities" },
-          status: { $first: "$status" },
-          location: { $first: "$location" },
-          created_at: { $first: "$created_at" },
-          typeOfProperty: { $first: "$typeOfProperty" },
-          property_photo: { $first: "$property_photo" }, // Group property photos
-          legal_docs: { $first: "$legal_docs" }, // Group legal document photos
-          address: { $first: "$address" },
-          profile: { $first: "$profile" },
-          rooms: { $push: "$rooms" }, // Push all rooms into an array
-        },
-      },
-    ]);
-
-    return result;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Service function to fetch all Approved requests and their profile data using aggregation
-const getAllApprovedListing = async () => {
-  try {
-    const result = await PropertyList.aggregate([
-      {
-        $match: {
-          status: "Approved",
-        },
-      },
-      {
-        $lookup: {
           from: "pending_request_profile",
           localField: "userId",
           foreignField: "userId",
@@ -177,6 +45,12 @@ const getAllApprovedListing = async () => {
           localField: "_id",
           foreignField: "propertyId",
           as: "rooms",
+        },
+      },
+      {
+        $unwind: {
+          path: "$rooms",
+          preserveNullAndEmptyArrays: true,
         },
       },
       {
@@ -234,7 +108,6 @@ const getAllApprovedListing = async () => {
             advance: "$rooms.advance",
             availability: "$rooms.availability",
           },
-          roomCount: { $size: "$rooms" }, // Count the number of rooms in the array
         },
       },
       {
@@ -250,24 +123,18 @@ const getAllApprovedListing = async () => {
           legal_docs: { $first: "$legal_docs" },
           address: { $first: "$address" },
           profile: { $first: "$profile" },
-          rooms: { $push: "$rooms" },
-          roomCount: { $max: "$roomCount" }, // Ensure roomCount is calculated correctly
-        },
-      },
-      {
-        $project: {
-          description: 1,
-          amenities: 1,
-          status: 1,
-          location: 1,
-          created_at: 1,
-          typeOfProperty: 1,
-          property_photo: 1,
-          legal_docs: 1,
-          address: 1,
-          profile: 1,
-          rooms: 1,
-          roomCount: 1,
+          rooms: {
+            $push: {
+              roomId: "$rooms.roomId",
+              roomNumber: "$rooms.roomNumber",
+              roomPhoto: "$rooms.roomPhoto",
+              capacity: "$rooms.capacity",
+              price: "$rooms.price",
+              deposit: "$rooms.deposit",
+              advance: "$rooms.advance",
+              availability: "$rooms.availability",
+            },
+          },
         },
       },
     ]);
@@ -278,17 +145,18 @@ const getAllApprovedListing = async () => {
   }
 };
 
-const getAllRejectedRequest = async () => {
+// Service function to fetch all Approved requests and their profile data using aggregation
+const getAllApprovedListing = async () => {
   try {
     const result = await PropertyList.aggregate([
       {
         $match: {
-          status: "Rejected", // Using $in to match multiple status values
+          status: "Approved",
         },
       },
       {
         $lookup: {
-          from: "pending_request_profile", // Ensure this collection matches your database
+          from: "pending_request_profile",
           localField: "userId",
           foreignField: "userId",
           as: "profile",
@@ -297,7 +165,7 @@ const getAllRejectedRequest = async () => {
       {
         $unwind: {
           path: "$profile",
-          preserveNullAndEmptyArrays: true, // Preserve listings without a matching profile
+          preserveNullAndEmptyArrays: true,
         },
       },
       {
@@ -311,13 +179,13 @@ const getAllRejectedRequest = async () => {
       {
         $unwind: {
           path: "$user",
-          preserveNullAndEmptyArrays: true, // Preserve listings without a matching user
+          preserveNullAndEmptyArrays: true,
         },
       },
       {
         $lookup: {
-          from: "rooms", // Ensure this collection matches your database
-          localField: "_id", // Use the property ID to look up related rooms
+          from: "rooms",
+          localField: "_id",
           foreignField: "propertyId",
           as: "rooms",
         },
@@ -325,7 +193,7 @@ const getAllRejectedRequest = async () => {
       {
         $unwind: {
           path: "$rooms",
-          preserveNullAndEmptyArrays: true, // Preserve listings without matching rooms
+          preserveNullAndEmptyArrays: true,
         },
       },
       {
@@ -337,7 +205,7 @@ const getAllRejectedRequest = async () => {
           typeOfProperty: 1,
           location: 1,
           address: {
-            $concat: ["$street", ", ", "$barangay", ", ", "$city"], // Concatenate firstName and lastName
+            $concat: ["$street", ", ", "$barangay", ", ", "$city"],
           },
           amenities: {
             $cond: {
@@ -350,31 +218,31 @@ const getAllRejectedRequest = async () => {
             $filter: {
               input: ["$photo", "$photo2", "$photo3"],
               as: "photo",
-              cond: { $ne: ["$$photo", null] }, // Exclude null photos
+              cond: { $ne: ["$$photo", null] },
             },
-          }, // Project property photos
+          },
           legal_docs: {
             $filter: {
               input: ["$legalDocPhoto", "$legalDocPhoto2", "$legalDocPhoto3"],
               as: "legaldocsPhoto",
-              cond: { $ne: ["$$legaldocsPhoto", null] }, // Exclude null room photos
-            }, // Project legal document photos
+              cond: { $ne: ["$$legaldocsPhoto", null] },
+            },
           },
           profile: {
             email: "$user.email",
             fullName: {
-              $concat: ["$profile.firstName", " ", "$profile.lastName"], // Concatenate firstName and lastName
+              $concat: ["$profile.firstName", " ", "$profile.lastName"],
             },
             contactDetails: "$profile.contactDetails",
           },
           rooms: {
-            roomId: "$rooms._id", // Project room details
+            roomId: "$rooms._id",
             roomNumber: "$rooms.roomNumber",
             roomPhoto: {
               $filter: {
                 input: ["$rooms.photo1", "$rooms.photo2", "$rooms.photo3"],
                 as: "roomPhoto",
-                cond: { $ne: ["$$roomPhoto", null] }, // Exclude null room photos
+                cond: { $ne: ["$$roomPhoto", null] },
               },
             },
             capacity: "$rooms.capacity",
@@ -387,18 +255,182 @@ const getAllRejectedRequest = async () => {
       },
       {
         $group: {
-          _id: "$_id", // Group by property ID to combine rooms into an array
+          _id: "$_id",
           description: { $first: "$description" },
           amenities: { $first: "$amenities" },
           status: { $first: "$status" },
           location: { $first: "$location" },
           created_at: { $first: "$created_at" },
           typeOfProperty: { $first: "$typeOfProperty" },
-          property_photo: { $first: "$property_photo" }, // Group property photos
-          legal_docs: { $first: "$legal_docs" }, // Group legal document photos
+          property_photo: { $first: "$property_photo" },
+          legal_docs: { $first: "$legal_docs" },
           address: { $first: "$address" },
           profile: { $first: "$profile" },
-          rooms: { $push: "$rooms" }, // Push all rooms into an array
+          rooms: {
+            $push: {
+              roomId: "$rooms.roomId",
+              roomNumber: "$rooms.roomNumber",
+              roomPhoto: "$rooms.roomPhoto",
+              capacity: "$rooms.capacity",
+              price: "$rooms.price",
+              deposit: "$rooms.deposit",
+              advance: "$rooms.advance",
+              availability: "$rooms.availability",
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          roomCount: { $size: "$rooms" }, // Add roomCount by calculating the size of the rooms array
+        },
+      },
+    ]);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+const getAllRejectedRequest = async () => {
+  try {
+    const result = await PropertyList.aggregate([
+      {
+        $match: {
+          status: "Rejected", // Using $in to match multiple status values
+        },
+      },
+      {
+        $lookup: {
+          from: "pending_request_profile",
+          localField: "userId",
+          foreignField: "userId",
+          as: "profile",
+        },
+      },
+      {
+        $unwind: {
+          path: "$profile",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: {
+          path: "$user",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "rooms",
+          localField: "_id",
+          foreignField: "propertyId",
+          as: "rooms",
+        },
+      },
+      {
+        $unwind: {
+          path: "$rooms",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          description: 1,
+          status: 1,
+          created_at: 1,
+          typeOfProperty: 1,
+          location: 1,
+          address: {
+            $concat: ["$street", ", ", "$barangay", ", ", "$city"],
+          },
+          amenities: {
+            $cond: {
+              if: { $isArray: "$amenities" },
+              then: "$amenities",
+              else: [],
+            },
+          },
+          property_photo: {
+            $filter: {
+              input: ["$photo", "$photo2", "$photo3"],
+              as: "photo",
+              cond: { $ne: ["$$photo", null] },
+            },
+          },
+          legal_docs: {
+            $filter: {
+              input: ["$legalDocPhoto", "$legalDocPhoto2", "$legalDocPhoto3"],
+              as: "legaldocsPhoto",
+              cond: { $ne: ["$$legaldocsPhoto", null] },
+            },
+          },
+          profile: {
+            email: "$user.email",
+            fullName: {
+              $concat: ["$profile.firstName", " ", "$profile.lastName"],
+            },
+            contactDetails: "$profile.contactDetails",
+          },
+          rooms: {
+            roomId: "$rooms._id",
+            roomNumber: "$rooms.roomNumber",
+            roomPhoto: {
+              $filter: {
+                input: ["$rooms.photo1", "$rooms.photo2", "$rooms.photo3"],
+                as: "roomPhoto",
+                cond: { $ne: ["$$roomPhoto", null] },
+              },
+            },
+            capacity: "$rooms.capacity",
+            price: "$rooms.price",
+            deposit: "$rooms.deposit",
+            advance: "$rooms.advance",
+            availability: "$rooms.availability",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          description: { $first: "$description" },
+          amenities: { $first: "$amenities" },
+          status: { $first: "$status" },
+          location: { $first: "$location" },
+          created_at: { $first: "$created_at" },
+          typeOfProperty: { $first: "$typeOfProperty" },
+          property_photo: { $first: "$property_photo" },
+          legal_docs: { $first: "$legal_docs" },
+          address: { $first: "$address" },
+          profile: { $first: "$profile" },
+          rooms: {
+            $push: {
+              roomId: "$rooms.roomId",
+              roomNumber: "$rooms.roomNumber",
+              roomPhoto: "$rooms.roomPhoto",
+              capacity: "$rooms.capacity",
+              price: "$rooms.price",
+              deposit: "$rooms.deposit",
+              advance: "$rooms.advance",
+              availability: "$rooms.availability",
+            },
+          },
+        },
+      },      
+      {
+        $addFields: {
+          roomCount: { $size: "$rooms" }, // Add roomCount by calculating the size of the rooms array
         },
       },
     ]);
